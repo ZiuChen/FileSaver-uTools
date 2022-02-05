93e3c821ce3b8fc4b3517e8803126dc279790874
ZiuChen <64892985+ZiuChen@users.noreply.github.com>
2022-02-06 00:38:07 +0800

    图片保存测试通过

--- publish/preload.js
+++ publish/preload.js
@@ -1,32 +1,119 @@
+const fs = require("fs");
 const defaultConfig = {
   "config-path": {
-      id: "config-path",
-      value: utools.getPath("downloads")
+    id: "config-path",
+    value: utools.getPath("downloads"),
   },
   "config-filename": {
-      id: "config-filename",
-      value: "uTools_" + "{ms_time_stamp}"
+    id: "config-filename",
+    value: "uTools_" + "{ms_time_stamp}",
   },
   "config-pictype": {
-      id: "config-pictype",
-      value: "origin"
+    id: "config-pictype",
+    value: "origin",
   },
   "config-silence": {
-      id: "config-silence",
-      value: false
-  }
+    id: "config-silence",
+    value: false,
+  },
 };
 
-window.updateConfig = function (isInit, config) {
-  if(isInit) {
-    utools.dbStorage.setItem("config", JSON.stringify(defaultConfig));
+const customFileNameConfigs = [
+  "{Y}",
+  "{M}",
+  "{D}",
+  "{h}",
+  "{m}",
+  "{s}",
+  "{ms_time_stamp}",
+  "{s_time_stamp}",
+];
+
+utools.onPluginEnter(({ code, type, payload }) => {
+  // utools.hideMainWindow();
+  console.log("用户进入插件", code, type, payload);
+  if (type === "img") {
+    savePicAsFile(payload);
   }
-  else {
+});
+
+function savePicAsFile(payload) {
+  let config = readConfig();
+  let path = `${getFilePath()}\\${getFileName()}.${getSuffix(payload)}`;
+  let base64Data = payload.replace(/^data:image\/\w+;base64,/, ""); // remove the prefix
+  let buffer = Buffer.from(base64Data, "base64"); // to Buffer
+  fs.writeFile(path, buffer, (err) => {
+    if (err !== null) {
+      utools.showNotification(err);
+      return;
+    } else {
+      // no error reported
+      if (config["config-silence"].value === true) {
+        return;
+      } else {
+        utools.shellShowItemInFolder(fullPath);
+      }
+    }
+  });
+}
+
+function getSuffix(payload) {
+  let config = readConfig();
+  if (config["config-pictype"].value === "origin") {
+    return payload.split("image/")[1].split(";base64,")[0];
+  } else {
+    return config["config-pictype"].value;
+  }
+}
+
+function getFilePath() {
+  let config = readConfig();
+  return config["config-path"].value;
+}
+
+function getFileName() {
+  let config = readConfig();
+  let originContent = config["config-filename"].value;
+  customFileNameConfigs.forEach((config) => {
+    let times = originContent.split(config).length - 1; // more than one replacement character
+    for (let i = 0; i < times; i++) {
+      originContent = originContent.replace(config, replacement(config));
+    }
+  });
+  return originContent;
+}
+
+function replacement(config) {
+  let date = new Date();
+  switch (config) {
+    case "{Y}":
+      return date.getFullYear();
+    case "{M}":
+      return date.getMonth() + 1;
+    case "{D}":
+      return date.getDay();
+    case "{h}":
+      return date.getHours();
+    case "{m}":
+      return date.getMinutes();
+    case "{s}":
+      return date.getSeconds();
+    case "{ms_time_stamp}":
+      return date.getTime();
+    case "{s_time_stamp}":
+      return parseInt(date.getTime() / 1000);
+  }
+}
+
+window.updateConfig = function (isInit, config) {
+  if (isInit) {
+    utools.dbStorage.setItem("config", JSON.stringify(defaultConfig));
+  } else {
     utools.dbStorage.setItem("config", JSON.stringify(config));
   }
   mdui.snackbar({
-    message: '设置已更新',
-    position: 'right-bottom',
+    message: "设置已更新",
+    position: "right-bottom",
   });
 };
 
@@ -36,5 +123,5 @@ window.readConfig = function () {
 };
 
 window.getDefaultConfig = function () {
-  return defaultConfig
-}
+  return defaultConfig;
+};
