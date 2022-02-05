a70b75e0c0787f47dbbb9829e1eccb597d6d18b6
ZiuChen <64892985+ZiuChen@users.noreply.github.com>
2022-02-05 22:39:59 +0800

    添加监听，实时更新设置

--- publish/src/main/js/main.js
+++ publish/src/main/js/main.js
@@ -2,6 +2,11 @@ const $ = mdui.$;
 
 utools.onPluginReady(() => {
   console.log("插件装配完成，已准备好");
+  initConfig();
+  checkConfigTableUpdate();
+});
+
+function initConfig() {
   if (readConfig() === null) {
     updateConfig(true);
   }
@@ -12,6 +17,28 @@ utools.onPluginReady(() => {
   $(".config-pictype").val(config["config-pictype"].value);
   let inst = new mdui.Select(".config-pictype");
   inst.handleUpdate();
-});
+}
 
-function updateTable() {}
+function checkConfigTableUpdate() {
+  let defaultConfig = getDefaultConfig();
+  Object.getOwnPropertyNames(defaultConfig).forEach((id) => {
+    console.log(id);
+    $(`.${id}`).on("change", (e) => {
+      updateConfigCallBack(e);
+    });
+  });
+}
+
+function updateConfigCallBack(event) {
+  let config = readConfig();
+  if (event.target.className.indexOf("config-path") !== -1) {
+    config["config-path"].value = event.target.value;
+  } else if (event.target.className.indexOf("config-filename") !== -1) {
+    config["config-filename"].value = event.target.value;
+  } else if (event.target.className.indexOf("config-pictype") !== -1) {
+    config["config-pictype"].value = event.target.value;
+  } else if (event.target.className.indexOf("config-silence") !== -1) {
+    config["config-silence"].value = event.target.checked;
+  }
+  updateConfig(false, config);
+}
